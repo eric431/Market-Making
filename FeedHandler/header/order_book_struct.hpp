856acb53ebd202dbf31c9805df79289546dff89b
eric431 <ericojaiyesimi@gmail.com>
2024-11-16 03:34:06 +0000

    order book struct library added to feed handler

---
+++ FeedHandler/header/order_book_struct.hpp
@@ -0,0 +1,61 @@
+#ifndef ORDER_BOOK_STRUCT_H
+#define ORDER_BOOK_STRUCT_H
+
+namespace MarketData
+{
+    struct Header
+    {
+
+    };
+
+    struct LogOn
+    {
+
+    };
+
+    struct MarketDataRequest
+    {
+
+    };
+
+    struct SecurityStatus
+    {
+
+    };
+
+    // Market Data Incremental Refresh
+    struct MDIR
+    {
+
+    };
+
+    // Market Data Snapshot Full Refresh
+    struct MDSFR
+    {
+
+    };
+
+    // Security List Refresh
+    struct SLR
+    {
+
+    };
+
+    // Security List
+    struct SL
+    {
+
+    };
+
+    struct MarketData
+    {
+
+    };
+}
+
+struct Fix
+{
+
+};
+
+#endif
