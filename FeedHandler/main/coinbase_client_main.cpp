fbe07f974bc7b7d82aae93e0b879b488e1f5eab6
eric431 <ericojaiyesimi@gmail.com>
2024-12-20 23:32:04 +0000

    coinbase_client commented code added for testing later

--- FeedHandler/main/coinbase_client_main.cpp
+++ FeedHandler/main/coinbase_client_main.cpp
@@ -5,18 +5,23 @@ using namespace Coinbase;
 
 static const std::string URI = "advanced-trade-ws.coinbase.com";
 
-int main()
+int main(int argc, char *argv[])
 {
     std::string host{URI};
     std::string port{"443"};
     std::string msg = R"({"type": "subscribe", "product_ids": ["BTC-USD"], "channel": "ticker"})";
 
     net::io_context ioc;
-
     ssl::context ctx{ssl::context::tlsv12_client};
 
-    // add loop to continuously get data
+    // auto coinbase_socket = std::make_shared<AsynchronousMarketDataClient>(ioc, ctx);
     std::make_shared<AsynchronousMarketDataClient>(ioc, ctx)->run(host, port, msg);
+
+    // while(true)
+    // {
+    //     coinbase_socket->run(host, port, msg);
+    //     break;
+    // }
 
     ioc.run();
 
